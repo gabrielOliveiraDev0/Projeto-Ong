6c323d92633c346b49fce7d5a8b7374f03a10df8
Gabriel Oliveira <gabrieloliveiradev0@gmail.com>
2026-09-11 18:06:53 -0400

    chore: criacao do componente formulario e separacao do mesmo

--- backend/src/main/java/com/projetoong/backend/controller/AnimalController.java
+++ backend/src/main/java/com/projetoong/backend/controller/AnimalController.java
@@ -1,14 +1,31 @@
 package com.projetoong.backend.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.projetoong.backend.model.Animal;
 import com.projetoong.backend.service.AnimalService;
 
 @RestController
 public class AnimalController {
+
     private final AnimalService animalService;
 
     public AnimalController(AnimalService animalService) {
         this.animalService = animalService;
     }
 
+    @GetMapping("/animais")
+    public List<Animal> listarAnimais() {
+        return animalService.listarAnimais();
+    }
+
+    @PostMapping("/animais")
+    public Animal salvarAnimal(@RequestBody Animal animal) {
+        return animalService.salvarAnimal(animal);
+    }
 }
